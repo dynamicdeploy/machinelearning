6a1536022e261df6798b4d0810ecca216895394b
Dynamic Deploy <dynamicdeploy@gmail.com>
2014-11-14 13:56:01 -0800

    First checkin

---
+++ RegressionAzureMLWP/AutoPredictionAzureMLClient.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace RegressionAzureMLWP
+{
+    public class AutoPredictionAzureMLClient
+    {
+        public static readonly string[] Make = new string[] {
+        "alfa-romero", "audi", "bmw", "chevrolet", "dodge", "honda",
+                               "isuzu", "jaguar", "mazda", "mercedes-benz", "mercury",
+                               "mitsubishi", "nissan", "peugot", "plymouth", "porsche",
+                               "renault", "saab", "subaru", "toyota", "volkswagen", "volvo"};
+
+        public static readonly string[] BodyStyle = new string[] { "hardtop", "sedan", "wagon", "hatchback", "convertible", ""};
+       
+        public const double WHEEL_BASE_MIN = 86;
+        public const double WHEEL_BASE_MAX = 121;
+        public static readonly string[] NumberOfCylinders = new string[] { "eight", "five", "four", "six", "three", "twelve", "two" };
+
+        public const int ENGINE_SIZE_MIN = 61;
+        public const int ENGINE_SIZE_MAX = 326;
+
+        public const int HORSEPOWER_MIN = 48;
+        public const int HORSEPOWER_MAX = 288;
+
+        public const int PEAK_RPM_MIN = 4150;
+        public const int PEAK_RPM_MAX = 6600;
+
+        public const int HIGHWAY_MPG_MIN = 16;
+        public const int HIGHWAY_MPG_MAX = 54;
+
+        public static async Task<string> InvokeRequestResponseService(string make, string bodyStyle, double wheelBase, string numOfCylinders, int engineSize, int horsepower, int peakRpm, int highwayMpg)
+        {
+            using (var client = new HttpClient())
+            {
+                ScoreData scoreData = new ScoreData()
+                {
+                    FeatureVector = new Dictionary<string, string>() 
+                    {
+                        { "make", make },
+                        { "body-style", bodyStyle },
+                        { "wheel-base", wheelBase.ToString() },
+                        { "num-of-cylinders", numOfCylinders },
+                        { "engine-size", engineSize.ToString() },
+                        { "horsepower", horsepower.ToString() },
+                        { "peak-rpm", peakRpm.ToString() },
+                        { "highway-mpg", highwayMpg.ToString() },
+                        { "price", "0" },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+
+                ScoreRequest scoreRequest = new ScoreRequest()
+                {
+                    Id = "score00001",
+                    Instance = scoreData
+                };
+
+                const string apiKey = "7EodnI2lplp2KGahJZzfwCHVE4e9NUNIwiWsOwq20QNtQgYKGENHGppRe7Vq9ruT5FwJCZ6ME0SnmiAoEAjFXg=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/5c51465da0e94cbcaeb05879306f7141/services/d70917b2d61245c7b819c250f6cac619/score");
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = await response.Content.ReadAsStringAsync();
+
+                   string[] rs = JsonConvert.DeserializeObject<string[]>(jsonString);
+
+                    if(rs != null && rs.Count() > 0)
+                    {
+                        return String.Format("{0:0.00}", (Double.Parse(rs[9])));
+                    }
+
+                    throw new Exception("Nothing returned.");
+
+                }
+                else
+                {
+                    throw new Exception(string.Format("Failed with status code: {0}", response.StatusCode));
+                }
+            }
+        }
+    }
+
+    public class ScoreData
+    {
+        public Dictionary<string, string> FeatureVector { get; set; }
+        public Dictionary<string, string> GlobalParameters { get; set; }
+    }
+
+    public class ScoreRequest
+    {
+        public string Id { get; set; }
+        public ScoreData Instance { get; set; }
+    }
+
+
+    public class Response
+    {
+        public string[] Properties { get; set; }
+    }
+
+}
+
+
+
+
+
---
+++ RegressionAzureMLWP/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace RegressionAzureMLWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            BindControl();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private void BindControl()
+        {
+            try
+            {
+                CbCylinders.ItemsSource = AutoPredictionAzureMLClient.NumberOfCylinders;
+
+                CbMake.ItemsSource = AutoPredictionAzureMLClient.Make;
+
+                CbBodyStyle.ItemsSource = AutoPredictionAzureMLClient.BodyStyle;
+                                            
+
+               
+            }
+            catch (Exception ex)
+            {
+                Windows.UI.Popups.MessageDialog md = new Windows.UI.Popups.MessageDialog(ex.Message);
+                md.ShowAsync();
+            }
+            
+        }
+        private async void OnPredict(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TxtPrice.Text = await AutoPredictionAzureMLClient.InvokeRequestResponseService(CbMake.SelectedItem.ToString(), CbBodyStyle.SelectedItem.ToString(),
+                    SliderWheelBase.Value, CbCylinders.SelectedItem.ToString(), (int)SliderEngineSize.Value, (int)SliderHP.Value, (int)SliderRPM.Value, (int)SliderMPG.Value);
+
+            }catch(Exception ex)
+            {
+                Windows.UI.Popups.MessageDialog md = new Windows.UI.Popups.MessageDialog(ex.Message);
+                md.ShowAsync();
+            }
+        }
+
+     
+
+      
+      
+    }
+}
